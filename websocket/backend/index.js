a354d939a10e000ccc2e96c73378edf9e3d82cdd
chevalierblu00 <mgaspard.perso@gmail.com>
2024-05-14 16:20:00 +0200

    flux video

--- websocket/backend/index.js
+++ websocket/backend/index.js
@@ -17,66 +17,67 @@ app.use(express.static(path.join(basePath, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(basePath, 'index.html'));
 });
+
 app.get('/room1', async (req, res) => {
-    const sockets = await io.in('Room1').allSockets();
-    if (sockets.size < 2) {
-        res.sendFile(path.join(__dirname, '..', 'front', 'room1.html'));
-    } else {
-        res.status(403).send('Cette room est pleine. Rejoins en une autre.');
-    }
+  const sockets = await io.in('Room1').allSockets();
+  if (sockets.size < 2) {
+    res.sendFile(path.join(__dirname, '..', 'front', 'room1.html'));
+  } else {
+    res.redirect('/?full=true');
+  }
 });
 
 app.get('/room2', async (req, res) => {
-    const sockets = await io.in('Room2').allSockets();
-    if (sockets.size < 2) {
-        res.sendFile(path.join(__dirname, '..', 'front', 'room2.html'));
-    } else {
-        res.status(403).send('Cette room est pleine. Rejoins en une autre.');
-    }
+  const sockets = await io.in('Room2').allSockets();
+  if (sockets.size < 2) {
+    res.sendFile(path.join(__dirname, '..', 'front', 'room2.html'));
+  } else {
+    res.redirect('/?full=true');
+  }
 });
 
 app.get('/roomCounts', async (req, res) => {
-    const room1Count = (await io.in('Room1').allSockets()).size;
-    const room2Count = (await io.in('Room2').allSockets()).size;
-    res.json({ Room1: room1Count, Room2: room2Count });
+  const room1Count = (await io.in('Room1').allSockets()).size;
+  const room2Count = (await io.in('Room2').allSockets()).size;
+  res.json({ Room1: room1Count, Room2: room2Count });
 });
 
 io.on('connection', (socket) => {
-    let roomJoined;
+  let roomJoined;
 
-    socket.on('joinRoom', async (room) => {
-        const sockets = await io.in(room).allSockets();
-        if (sockets.size < 2) {
-            socket.join(room);
-            roomJoined = room;
-            console.log(`User ${socket.id} joined room: ${room}`);
-            io.to(socket.id).emit('roomJoined', { room: room, status: 'joined' });
-            socket.to(room).emit('receiveMessage', {
-                message: `A rejoint la salle.`,
-                sender: 'System'
-            });
-        } else {
-            console.log(`Room ${room} is full`);
-            io.to(socket.id).emit('roomFull', { room: room, status: 'full' });
-        }
-    });
+  socket.on('joinRoom', async (room) => {
+    const sockets = await io.in(room).allSockets();
+    if (sockets.size < 2) {
+      socket.join(room);
+      roomJoined = room;
+      console.log(`User ${socket.id} joined room: ${room}`);
+      io.to(socket.id).emit('roomJoined', { room: room, status: 'joined' });
+      socket.to(room).emit('receiveMessage', {
+        message: `A rejoint la salle.`,
+        sender: 'System'
+      });
+    } else {
+      console.log(`Room ${room} is full`);
+      io.to(socket.id).emit('roomFull', { room: room, status: 'full' });
+    }
+  });
 
-    socket.on('sendMessage', (data) => {
-        const { room, message } = data;
-        io.to(room).emit('receiveMessage', { message: message, sender: socket.id });
-    });
+  socket.on('sendMessage', (data) => {
+    const { room, message } = data;
+    io.to(room).emit('receiveMessage', { message: message, sender: socket.id });
+  });
 
-    socket.on('disconnect', () => {
-        console.log(`User ${socket.id} disconnected`);
-        if (roomJoined) {
-            io.in(roomJoined).emit('receiveMessage', {
-                message: `A quitté la salle.`,
-                sender: 'System'
-            });
-        }
-    });
+  socket.on('disconnect', () => {
+    console.log(`User ${socket.id} disconnected`);
+    if (roomJoined) {
+      io.in(roomJoined).emit('receiveMessage', {
+        message: `A quitté la salle.`,
+        sender: 'System'
+      });
+      socket.to(roomJoined).emit('user-disconnected', socket.id);
+    }
+  });
 });
-
 
 server.listen(3000, () => {
   console.log('Server listening on :3000');
